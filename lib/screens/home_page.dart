5e176a8c5eea2788ed476e841db269981f90da8b
Arthur <arthur.adnot2@gmail.com>
2024-03-20 17:25:18 +0100

    l

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import '../models/villes.dart';
 import '../widgets/city_search.dart';
 import '../models/ville_france.dart';
+import 'package:fl_chart/fl_chart.dart';
+import '../const/const.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key? key, required this.title}) : super(key: key);
@@ -20,12 +22,12 @@ class _HomePageState extends State<HomePage> {
   late Future<List<Ville>> villes;
   late List<Ville> allVilles = [];
 
-  callback(Ville varCity, dataVille data) {
-    setState(() {
+  callback(Ville varCity,dataVille data) {
+    setState(()  {
       city = varCity.nomAvecArticle!;
       selectedVille = varCity;
-      time = data.hourlyTime;
-      temp = data.hourlyTemp;
+      time=data.hourlyTime;
+      temp=data.hourlyTemp;
     });
   }
 
@@ -37,21 +39,7 @@ class _HomePageState extends State<HomePage> {
     villes.then((value) {
       setState(() {
         allVilles = value;
-        // Récupérer les données de la ville Ivry-sur-Seine une fois que la liste des villes est chargée
-        Ville ivrySurSeine = allVilles.firstWhere((ville) => ville.nomAvecArticle == 'Ivry-sur-Seine');
-        getDataForCity(ivrySurSeine);
       });
-    });
-  }
-
-  void getDataForCity(Ville city) async {
-    final data = await GetDataVille(city.latitude, city.longitude).getData();
-    setState(() {
-      selectedVille = city;
-      time = data.hourlyTime;
-      temp = data.hourlyTemp;
-      // Affichez les données de la ville directement une fois récupérées
-      city = selectedVille!;
     });
   }
 
@@ -67,6 +55,7 @@ class _HomePageState extends State<HomePage> {
     var screenSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
+
         toolbarHeight: 72,
         centerTitle: true,
         title: Text("La Météo"),
@@ -77,67 +66,74 @@ class _HomePageState extends State<HomePage> {
               padding: EdgeInsets.all(8),
               child: Column(
                 children: <Widget>[
+
                   CitySearch(callback: callback, villes: allVilles),
                 ],
               ),
             ),
           )
         ],
-        backgroundColor: Color(0xFF637E92),
+
+        backgroundColor: Color(0xFF637E92), //Appbar color
+
       ),
       body: Container(
-        margin: const EdgeInsets.symmetric(vertical: 20),
-        height: 2000,
+
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Colors.blue.shade200, Colors.blue.shade50],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
         child: ListView(
           scrollDirection: Axis.horizontal,
           children: <Widget>[
             Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                color: Color(0xFF637E92),
-              ),
-              padding: EdgeInsets.fromLTRB(10, 5, 20, 20),
-              width: screenSize.width - 50,
-              margin: EdgeInsets.all(10),
-              child: selectedVille != null
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    color: Color(0xFF637E92).withOpacity(0.4), //couleur bulle 1
+                ),
+                padding: EdgeInsets.fromLTRB(10, 5, 20, 20),
+                width: screenSize.width-100,
+                margin: EdgeInsets.all(50),
+                child: selectedVille != null
                   ? Wrap(
-                direction: Axis.horizontal,
-                spacing: 8.0,
-                runSpacing: 4.0,
-                children: [
-                  Text("Aujourd'hui\t"),
-                  Text(selectedVille!.nomAvecArticle!),
-                  for (var heure in time.toList()[1])
-                    Text(heure.toString() + ""),
-                  for (var temperature in temp.toList()[1])
-                    Text(temperature.toString() + ""),
+                  direction: Axis.horizontal,
+                  spacing: 8.0,
+                  runSpacing: 4.0,
+                  children: [
+                    Text("Aujourd'hui\t"),
+                    Text(selectedVille!.nomAvecArticle!),
+                    //Text("Longitude: ${selectedVille!.longitude}"),
+                   // Text("Latitude: ${selectedVille!.latitude}"),
+                    for (var heure in time.toList()[1]) Text(heure.toString()+""),
+                    for (var temperature in temp.toList()[1]) Text(temperature.toString()+""),
                 ],
-              )
-                  : Text(city),
+              ):Text(city),
             ),
             Container(
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(10)),
-                color: Color(0xFF637E92),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  color: Color(0xFF637E92).withOpacity(0.4)
               ),
               padding: EdgeInsets.fromLTRB(10, 5, 20, 20),
-              width: screenSize.width - 50,
-              margin: EdgeInsets.all(10),
+              width: screenSize.width-100,
+              margin: EdgeInsets.all(50),
               child: selectedVille != null
                   ? Wrap(
-                direction: Axis.horizontal,
-                spacing: 8.0,
-                runSpacing: 4.0,
-                children: [
-                  Text("Demain\t"),
-                  Text(selectedVille!.nomAvecArticle!),
-                  for (var heure in time.toList()[2])
-                    Text(heure.toString() + ""),
-                  for (var temperature in temp.toList()[2])
-                    Text(temperature.toString() + ""),
+                  direction: Axis.horizontal,
+                  spacing: 8.0,
+                  runSpacing: 4.0,
+                  children: [
+                    Text("Demain\t"),
+                    Text(selectedVille!.nomAvecArticle!),
+                    //Text("Longitude: ${selectedVille!.longitude}"),
+                    //Text("Latitude: ${selectedVille!.latitude}"),
+                    for (var heure in time.toList()[2]) Text(heure.toString()+""),
+                    for (var temperature in temp.toList()[2]) Text(temperature.toString()+""),
                 ],
-              )
-                  : Text(city),
+              ):Text(city),
             ),
           ],
         ),
