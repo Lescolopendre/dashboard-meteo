45da3119349dda223019f6cfd7c1fafee2d80752
meluxis <melo.desbos@gmail.com>
2024-03-26 17:59:25 +0100

    in process air quality

--- lib/screens/bottom_left.dart
+++ lib/screens/bottom_left.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import '../screens/home_page.dart';
 import 'package:weather_icons/weather_icons.dart';
 import '../models/air_quality.dart';
 
