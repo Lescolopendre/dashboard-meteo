ac2ae6479811afb6a15b967fd3c906756d9d9312
meluxis <melo.desbos@gmail.com>
2024-03-31 18:13:16 +0200

    en cours..

--- lib/screens/bottom_left.dart
+++ lib/screens/bottom_left.dart
@@ -26,144 +26,119 @@ class getContentBottomContainers extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        margin: EdgeInsets.all(0.5),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-        ),
-        child: Column(
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                ),
-                height: 85,
-                width: double.infinity,
-                // Pour occuper toute la largeur de la colonne
-                margin: EdgeInsets.all(3),
-                child: Row(
-                    children: [
-                      Expanded(child:
-                      Container( //container temp apparente et wind speed
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
-                        ),
-                        height: 85,
-                        margin: EdgeInsets.all(0.5),
-                        child: Column(
-                            children: [
-                              Expanded(child:
-                              Container( //container des temperature apparente
-                                  decoration: BoxDecoration(
-                                    color: Color(0xFF07549D).withOpacity(0.1),
-                                    borderRadius: BorderRadius.all(
-                                        Radius.circular(10)),
+      margin: EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch, // Stretch children horizontally
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(3),
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Color(0xFF07549D).withOpacity(0.1),
+                              borderRadius: BorderRadius.all(Radius.circular(10)),
+                            ),
+                            margin: EdgeInsets.all(3),
+                            child: Center(
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    (hourlyApparentTemp.toList()[0][currentHour]).toString() + " °C",
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      fontWeight: FontWeight.bold,
+                                      fontFamily: 'Roboto',
+                                      color: Colors.white,
+                                    ),
                                   ),
-                                  height: 50,
-                                  margin: EdgeInsets.all(0.5),
-                                  child:
-                                    //ajouter titre et icone
-                                    Text((hourlyApparentTemp
-                                        .toList()[0][currentHour]).toString() +
-                                        " °C",
-                                      style: TextStyle(
-                                        fontSize: 15, // Taille de la police
-                                        fontWeight: FontWeight.bold, // Poids de la police
-                                        fontFamily: 'Roboto', // Famille de police
-                                        color: Colors.white, // Couleur du texte
-                                      ),)
-
+                                ],
                               ),
-                              ),
-                              Expanded(child:
-                              Container( //container wind speed
-                                decoration: BoxDecoration(
-                                  color: Color(0xFF07549D).withOpacity(0.1),
-                                  borderRadius: BorderRadius.all(
-                                      Radius.circular(10)),
-                                ),
-                                height: 34,
-                                margin: EdgeInsets.all(0.5),
-                                child:
-                                    //ajout titre et icon vitesse vent
-                                  Text((hourlyWindSpeed.toList()[0][currentHour]).toString()+"km/h")
-                              ),
-                              ),
-                            ]
+                            ),
+                          ),
                         ),
 
-                      ),
-                      ),
-                      Expanded(child:
-                      Container( //humidite et uv display
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Color(0xFF07549D).withOpacity(0.1),
+                              borderRadius: BorderRadius.all(Radius.circular(10)),
+                            ),
+                            margin: EdgeInsets.all(3),
+                            child: Center(
+                              child: Text((hourlyWindSpeed.toList()[0][currentHour]).toString() + " km/h"),
+                            ),
+                          ),
                         ),
-                        height: 85,
-                        margin: EdgeInsets.all(0.5),
-                        child: Column(
-                            children: [
-                              Expanded(child:
-                              Container( //container humidity display
-                                decoration: BoxDecoration(
-                                  color: Color(0xFF07549D).withOpacity(0.1),
-                                  borderRadius: BorderRadius.all(
-                                      Radius.circular(10)),
-                                ),
-                                height: 50,
-                                margin: EdgeInsets.all(0.5),
-                                child: Row(
-                                  children: <Widget>[
-                                    Icon(WeatherIcons.humidity,
-                                        color: Colors.blue.shade100),
-                                    SizedBox(width: 5),
-                                    Text((hourlyHumidity.toList()[0][currentHour]).toString()+" %"),
-                                  ],
-                                ),
-                              ),
-                              ),
-                              Expanded(child:
-                              Container( //container uv display
-                                decoration: BoxDecoration(
-                                  color: Color(0xFF07549D).withOpacity(0.1),
-                                  borderRadius: BorderRadius.all(
-                                      Radius.circular(10)),
-
-                                ),
-                                height: 34,
-                                margin: EdgeInsets.all(0.5),
-                                child: Row(
-                                  children: <Widget>[
-                                    Icon(WeatherIcons.day_lightning, size: 20,
-                                        color: Colors.red),
-                                    SizedBox(width: 8),
-                                    Text((hourlyUVIndex.toList()[0][currentHour]).toString()),
-                                  ],
-                                ),
-                              ),
-                              ),
-                            ]
-                        ),
-                      ),
-                      ),
-                    ]
+                      ],
+                    ),
+                  ),
                 ),
-
-
-              ),
-             Container( //pollution display
-                decoration: BoxDecoration(
-                    color: Color(0xFF07549D).withOpacity(0.1),
-                    borderRadius: BorderRadius.all(Radius.circular(10))
+                Expanded(
+                  child: Container(
+                    margin: EdgeInsets.all(3),
+                    child: Column(
+                      children: [
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Color(0xFF07549D).withOpacity(0.1),
+                              borderRadius: BorderRadius.all(Radius.circular(10)),
+                            ),
+                            margin: EdgeInsets.all(3),
+                            child: Row(
+                              children: <Widget>[
+                                Icon(WeatherIcons.humidity, color: Colors.blue.shade100),
+                                SizedBox(width: 5),
+                                Text((hourlyHumidity.toList()[0][currentHour]).toString() + " %"),
+                              ],
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            decoration: BoxDecoration(
+                              color: Color(0xFF07549D).withOpacity(0.1),
+                              borderRadius: BorderRadius.all(Radius.circular(10)),
+                            ),
+                            margin: EdgeInsets.all(3),
+                            child: Row(
+                              children: <Widget>[
+                                Icon(WeatherIcons.day_lightning, size: 20, color: Colors.red),
+                                SizedBox(width: 8),
+                                Text((hourlyUVIndex.toList()[0][currentHour]).toString()),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
-                height: 85,
-                width: double.infinity,
-                margin: EdgeInsets.all(3),
-                child:
-                  //Text((hourlyAqi.toList()[0][currentHour]).toString()),
-                  getPollutionIndicator(currentHour: currentHour, hourlyAqi: hourlyAqi,),
-              ),
-            ]
-        )
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(3),
+            decoration: BoxDecoration(
+              color: Color(0xFF07549D).withOpacity(0.1),
+              borderRadius: BorderRadius.all(Radius.circular(10)),
+            ),
+            child: getPollutionIndicator(currentHour: currentHour, hourlyAqi: hourlyAqi,),
+          ),
+        ],
+      ),
     );
+
+
   }
 }
